dc531fcdcf4beff145bf87a519ef82f76049f7c2
shariarf39 <shariarf39@gmail.com>
2023-02-24 00:44:46 +0600

    Fab Blood Bank

--- app/src/main/java/com/example/fabred/Log_in.java
+++ app/src/main/java/com/example/fabred/Log_in.java
@@ -2,13 +2,26 @@ package com.example.fabred;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Log_in extends AppCompatActivity {
+
+    soup.neumorphism.NeumorphTextView registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        registration = findViewById(R.id.registration);
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Log_in.this, Registration_activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/fabred/MainActivity.java
+++ app/src/main/java/com/example/fabred/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.fabred;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int PLASH_TIMER=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent myIntent = new Intent(MainActivity.this, Log_in.class);
+
+                startActivity(myIntent);
+                finish();
+
+
+
+            }
+        },PLASH_TIMER);
+
     }
 }
